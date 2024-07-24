2f7e7da0841b27c5cd41842a786a14d771aed395
jormundgandr <business.nimh@outlook.com>
2024-07-24 16:34:19 -0500

    database conection docker

--- config.js
+++ config.js
@@ -5,5 +5,12 @@ module.exports={
     ,
     jwt:{
         secret : process.env.SECRET || 'secret'
-    }
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'user',
+        password: process.env.MYSQL_PASS || 'password',
+        database: process.env.MYSQL_DB || 'db',
+        port: process.env.MYSQL_PORT || 3306
+    },
 }
